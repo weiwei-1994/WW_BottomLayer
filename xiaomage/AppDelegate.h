f145c09a4b28e7bb5fbf33e86c29ccb0a587192f
“weiwei-1994” <13293346107@163.com>
2022-08-03 15:53:14 +0800

    Initial Commit

---
+++ xiaomage/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  xiaomage
+//
+//  Created by wyzeww on 2022/8/3.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ xiaomage/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  xiaomage
+//
+//  Created by wyzeww on 2022/8/3.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
